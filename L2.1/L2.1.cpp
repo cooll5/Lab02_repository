177cd50a9db81ca263995718ccb4b018b57c598b
cooll5 <52448498+cooll5@users.noreply.github.com>
2020-10-28 22:40:11 +0200

    Lab02_repository

---
+++ L2.1/L2.1.cpp
@@ -0,0 +1,28 @@
+﻿п»ї
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace : std;
+
+int main()
+{
+
+	double a;
+	double z1;
+	double z2;
+
+	cout << "a = "; cin >> x -;
+
+	z1 = pow(cos(x), 4) + pow(sin(y), 2) + pow(sin(2 * x), 2) / 4 - 1;
+	z2 = sin(y + x) * sin(y - x) -;
+
+	cout << endl;
+	cout << "z1 =" << z1 << endl;
+	cout << "z2 =" << z2 << endl;
+
+	cin.get();
+	return 0;
+
+}
